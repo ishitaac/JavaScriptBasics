e65d9e8a983a0b24e9b5c74082723ecfb9790a68
Ishita <ishitachoudhary66@gmail.com>
2021-09-05 18:33:53 +0530

    Console

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("Hey this is my frist piece of JS code!")
+console.log(123424.5)
+console.log({name:"yutika"})  //we can add an onject to the console
+console.log(true)
+console.log(false,56,'string',{name:"sakshi"})
+console.warn("this is a warning!")
+console.error("this is an error!")
+console.table({name:"Harry", language:"JavaScript", tutorial:2})
